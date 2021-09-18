419fa760d085743aeb5a45ff7df2ad36d36c8402
Elena P <elena.pan.ep@gmail.com>
2021-09-18 19:25:08 -0400

    cleanup

--- routes/api/datapoints.js
+++ routes/api/datapoints.js
@@ -7,21 +7,20 @@ router.get("",
     //passport.authenticate("jwt", { session: false }), 
     async (req, res) => {
         const user = { 
-            id: "test123", 
             username: user.username 
         };
-        console.log("hello");
         result = {
             "timedata": [],
-            "actualization": 0,
-            "esteeem": 0,
-            "belongingness": 0,
-            "safety": 0,
-            "physiological": 0,
+            "maslows": {
+                "actualization": 0,
+                "esteeem": 0,
+                "belongingness": 0,
+                "safety": 0,
+                "physiological": 0,
+            }
 
         }
-        console.log("here");
-        datapoint.find({ userid: req.user.userid},
+        datapoint.find({ username: req.user.username},
             function (err, docs) {
                 console.log("gathered datapoints; processing")
                 counter = 0
@@ -33,58 +32,34 @@ router.get("",
                         "sleepquality": element.sleepquality
                     }
                     result.timedata.push(datedata);
-                    result.actualization += element.actualization;
-                    result.esteeem += element.esteem;
-                    result.belongingness += element.belongingness;
-                    result.safety += element.safety;
-                    result.physiological += element.physiological;
+                    result.maslows.actualization += element.actualization;
+                    result.maslows.esteeem += element.esteem;
+                    result.maslows.belongingness += element.belongingness;
+                    result.maslows.safety += element.safety;
+                    result.maslows.physiological += element.physiological;
                 });
         });
         console.log("datapoints processed");
         try {
-            result.actualization /= result.timedata.length();
-            result.esteem /= result.timedata.length();
-            result.belongingness /= result.timedata.length();
-            result.safety /= result.timedata.length();
-            result.physiological /= result.timedata.length();
+            result.maslows.actualization /= result.timedata.length();
+            result.maslows.esteem /= result.timedata.length();
+            result.maslows.belongingness /= result.timedata.length();
+            result.maslows.safety /= result.timedata.length();
+            result.maslows.physiological /= result.timedata.length();
         } catch (error) {
             result = {
                 "timedata": [],
-                "actualization": 0,
-                "esteeem": 0,
-                "belongingness": 0,
-                "safety": 0,
-                "physiological": 0,
+                "maslows": {
+                    "actualization": 0,
+                    "esteeem": 0,
+                    "belongingness": 0,
+                    "safety": 0,
+                    "physiological": 0
+                }
             }
         }
         console.log("done!");
         res.json(result);
-/*
-        {
-            timedata: [
-                {
-                    userid: 1,
-                    date: new Date("2021-09-11"),
-                    notes: "kjsdlf;fs;sdfjoisfoijfojifoijf",
-                    sleeptime: 12,
-                    sleepquality: 5
-                },
-                {
-                    userid: 1,
-                    date: new Date("2021-09-12"),
-                    notes: "wheeeeeeeeeeeeeeeeeeeeeee eeeee ee e e e e",
-                    sleeptime: 9,
-                    sleepquality: 4
-                },
-            ],
-            maslows: {
-                actualization: 2,
-                esteem: 3,
-                belongingness: 9,
-                safety: 10,
-                physiological: 6
-            }
-        }*/
 });
 
 module.exports = router;
