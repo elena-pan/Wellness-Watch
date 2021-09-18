425cef17ba5a77e15b93f103ab8007c600165c77
Dhananjay Patki <dpatki64@gmail.com>
2021-09-18 18:31:19 -0400

    added data upload

--- routes/api/datapoints.js
+++ routes/api/datapoints.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const passport = require("passport");
-const datapoint = require('../../models/Datapoint');
+const Datapoint = require('../../models/zDatapoint');
+const datapoint = require('../../models/zDatapoint');
 //const User = require("../../models/User");
 
 router.get("",
@@ -8,7 +9,7 @@ router.get("",
     async (req, res) => {
         const user = { 
             id: "test123", 
-            username: user.username 
+            username: "test123"
         };
         console.log("hello");
         result = {
@@ -21,8 +22,8 @@ router.get("",
 
         }
         console.log("here");
-        datapoint.find({ userid: req.user.userid},
-            function (err, docs) {
+        datapoint.find({ userid: user.id})
+            .then(docs => {
                 console.log("gathered datapoints; processing")
                 counter = 0
                 docs.array.forEach(element => {
@@ -39,26 +40,21 @@ router.get("",
                     result.safety += element.safety;
                     result.physiological += element.physiological;
                 });
-        });
+                if (counter != 0) {
+                    result.actualization /= result.timedata.length();
+                    result.esteem /= result.timedata.length();
+                    result.belongingness /= result.timedata.length();
+                    result.safety /= result.timedata.length();
+                    result.physiological /= result.timedata.length();
+                }
+            })
+            .catch(err => console.log(err))
         console.log("datapoints processed");
-        try {
-            result.actualization /= result.timedata.length();
-            result.esteem /= result.timedata.length();
-            result.belongingness /= result.timedata.length();
-            result.safety /= result.timedata.length();
-            result.physiological /= result.timedata.length();
-        } catch (error) {
-            result = {
-                "timedata": [],
-                "actualization": 0,
-                "esteeem": 0,
-                "belongingness": 0,
-                "safety": 0,
-                "physiological": 0,
-            }
-        }
+        
+            
         console.log("done!");
         res.json(result);
+
 /*
         {
             timedata: [
@@ -85,6 +81,40 @@ router.get("",
                 physiological: 6
             }
         }*/
+});
+
+router.post("/upload",
+    //passport.authenticate("jwt", { session: false }), 
+    async (req, res) => {
+        const user = { 
+            id: 13, 
+            username: "test123"
+        };
+        console.log("hello");
+        Datapoint.findOne({ "userid": user.id, "date": req.date}).then(document => {    
+            if (document) {
+                return res.status(400).json({ error: "Data for this date exists" });
+            } else {
+                const newData = new Datapoint({
+                    userid: 123,
+                    date: Date.now(),
+                    notes: "hello",
+                    sleeptime: 8,
+                    sleepquality: 5,
+                    actualization: 2,
+                    esteem: 3,
+                    belongingness: 4,
+                    safety: 5,
+                    physiological: 6
+                });
+                newData.save()
+                      .then(document => {
+                          res.status(200).json({ success: true })
+                      })
+                      .catch(err => console.log(err)); 
+            }
+        });
+        console.log("success??");
 });
 
 module.exports = router;
