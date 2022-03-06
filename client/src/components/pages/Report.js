985059108f08db061d61bc2e2a7e3789857a9947
dpatki <dpatki64@gmail.com>
2022-03-05 20:13:26 -0700

    Modified report page with better analytics

--- client/src/components/pages/Report.js
+++ client/src/components/pages/Report.js
@@ -1,29 +1,90 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import LinearLoadingSymbol from "../layout/LinearLoadingSymbol";
 import axios from "axios"
 
 function Report(props) {
+    const pastDate = new Date(new Date().getTime() - 7 * (24 * 60 * 60 * 1000));
+    const currentDate = new Date(new Date().setHours(0,0,0,0));
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+
+    const [averages, setAverages] = useState(null);
+    const [sleepTime, setSleepTime] = useState(null);
+    const [sleepQuality, setSleepQuality] = useState(null);
+
+    async function getData() {
+        axios.get(`${serverUrl}/api/datapoints?startDate=${pastDate}&endDate=${currentDate}`).then((result) => {
+            console.log(result);
+            setAverages(result.data.maslows);
+            let calcTime = 0;
+            let calcQuality = 0;
+            if (result.data.timedata.length !== 0) {
+                result.data.timedata.forEach(element => {
+                    calcTime += element.sleeptime / result.data.timedata.length;
+                    calcQuality += element.sleepquality / result.data.timedata.length;
+                });
+            }
+
+            setSleepQuality(calcQuality);
+            setSleepTime(calcTime);
+           
+        })
+    }
+    
+    useEffect(() => {
+        getData().catch(err => console.log(err));
+    }, [setAverages, setSleepTime, setSleepQuality])
+
+    let content;
+
+    if (averages == null) {
+        return (<React.Fragment>
+            <i className="material-icons logo">public</i>
+            <LinearLoadingSymbol />
+            <h5 className="grey-text text-darken-2">
+                Loading...
+            </h5>
+        </React.Fragment>);
+    }
     return (
+
+
         <div className="container">
             <div style={{ marginTop: "4rem", marginBottom:"5rem" }} className="row">
                 <div className="col s8 offset-s2">
                     <h4>
                         <b>Wellness Report</b>
                     </h4>
-                    <p className="grey-text">September 12-19</p>
+                    <p className="grey-text">Report for {pastDate.toDateString()} - {currentDate.toDateString()}</p>
                 </div>
                 <div className="col s8 offset-s2">
                     <p style={{marginTop: "1rem"}}><i>Needs</i></p>
-                    <p>Your physiological and safety needs were well met this week, rated <b>8/10</b> on average - these are basic needs like food, water, and security.
-                        Belongingness needs, which are the emotional need for interpersonal relationships, were not fully met this week.
-                        Your self-actualization needs the most work. This is the highest level of Maslow's hierarchy and describes your self-fulfillment,
-                        and sense of accomplishment. Overall, you rated your self-actualization and belongingness needs as <b>27%</b> less fulfilled than your physiological and security needs.
+                    <p>Your physiological and safety needs were {(averages.physiological + averages.safety) > 10 ? "well" : "poorly"} met this week,
+                        rated <b>{averages.physiological} and {averages.safety}</b> respectively - these are basic needs like food, water, and security.
+
+                        Belongingness needs, which are the emotional need for interpersonal relationships, were {(averages.belonging) > 5 ? "" : "not"} fully met this week, 
+                        with a rating of <b>{averages.belongingness}</b>.
+
+                        Esteem was a {averages.esteem > 5 ? "high" : "low"} point this week, rated <b>{averages.esteem}</b>. 
+                        Your self-actualization {averages.actualization > 5 ? "was high this week" : "could use more work"}, with a rating of {averages.actualization}. 
+                        This is the highest level of Maslow's hierarchy and describes your self-fulfillment, and sense of accomplishment. 
+                        
+                        Overall, you rated your self-actualization and belongingness needs as 
+                        <b>{(averages.physiological + averages.safety) > (averages.actualization + averages.belonging) ? "less" : "more"}</b> 
+                        fulfilled than your physiological and security needs.
                     </p>
 
                     <p style={{marginTop: "2rem"}}><i>Sleep</i></p>
-                    <p>You slept <b>30%</b> more hours compared to last week and saw a <b>7%</b> increase in sleep quality! 
-                    You get about 20% more sleep on weekends compared to weekdays. Saturdays are when you get the most sleep, 
-                    and Wednesdays are when you get the least sleep.</p>
+                    <p>You slept <b>{sleepTime}</b> hours this week, with a qualtiy of <b>{sleepQuality}</b>! 
+                    {(sleepTime > 10 || sleepTime < 7) ? 
+                    ` That amount of sleep could use a bit more consistency - it's highly recommended to get at least 7 hours of sleep, on average. 
+                    Sleeping more than 10 hours is also generally detrimental. ` 
+                    : 
+                    ` That's quite consistent for sleep time - Keep up the good work. `} 
+                    {sleepQuality <= 5 ? 
+                    ` Your sleep quality was quite poor this week - try and maintain a consistent sleep scheudle, and ensure you have good sleep hygine. `
+                    : 
+                    ` Your sleep quality was doing well this week, so keep up those good habits! `}
+                    </p>
 
                     <p style={{marginTop: "2rem"}}><i>Recommendations</i></p>
                     <p>To fulfill your belongingness needs, some simple ways to connect can be to meet up with a friend for coffee,
